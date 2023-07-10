8bec50e21e72a4e3e673745a31ba3a94b468b267
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2023-07-10 18:18:43 -0300

    Front-end Revamp
    Revamped the code referent to the front-end and also added images and a field to the "sleeved" status

--- CardKeeper-Front/src/script.js
+++ CardKeeper-Front/src/script.js
@@ -1,37 +1,66 @@
 
-fetch('http://localhost:8080/cards')
-.then(res => res.json())
-.then(data => {
-    let cardData = data;
-    data.map(function(cardData){
-        let main = document.getElementById("mainRow")
- 
-        let col = document.createElement("div");
-        let card = document.createElement("div")
-        let cardBody = document.createElement("div");
-        
-        let cardName = document.createElement("p")
-        let cardCode = document.createElement("p")
-        let cardsAmount = document.createElement("p")
-        let cardQuality = document.createElement("p")
-        
-        cardName.innerHTML = `Name: ${cardData.CardName}`
-        cardCode.innerHTML = `Code: ${cardData.CardCode}`
-        cardsAmount.innerHTML = `Number of cards: ${cardData.CardsAmount}`
-        cardQuality.innerHTML = `Card Quality: ${cardData.CardQuality}`
-        col.classList.add("col-2")
-        col.classList.add("text-center")
-        card.classList.add("card")
-        card.classList.add("bg-light")
-        cardBody.classList.add("card-body")
-       
-        cardBody.appendChild(cardName)
-        cardBody.appendChild(cardCode)
-        cardBody.appendChild(cardsAmount)
-        cardBody.appendChild(cardQuality)
+const url = 'http://localhost:8080/cards';
+async function getAllCards(){
+    const response = await fetch(url);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    data.map((card) => cardBuilder(card))
+}
 
-        card.appendChild(cardBody)
-        col.appendChild(card)
-        main.appendChild(col)
-    })
-})
+function cardBuilder(cardData){
+    let main = document.getElementById("mainRow")
+
+    let col = document.createElement("div");
+    let card = document.createElement("div")
+    let cardBody = document.createElement("div");
+    let cardText = document.createElement("div")
+    let cardImage = document.createElement("img")
+    
+   
+    let cardName = document.createElement("p")
+    let cardCode = document.createElement("p")
+    let cardsAmount = document.createElement("p")
+    let cardQuality = document.createElement("p")
+    let cardSleeve = document.createElement("p")
+    
+    //This will get the json field in the future
+    cardImage.src = "https://www.duelshop.com.br/5728-large_default/blue-eyes-white-dragon-ldk2-enk01-common.jpg"
+    cardName.innerHTML = `Name: ${cardData.CardName}`
+    cardCode.innerHTML = `Code: ${cardData.CardCode}`
+    cardsAmount.innerHTML = `Number of cards: ${cardData.CardsAmount}`
+    cardQuality.innerHTML = `Card Quality: ${cardData.CardQuality}`
+    //This will get the json field in the future
+    cardSleeve.innerHTML = `Card Sleeved: Yes`
+
+
+    cardText.classList.add("card-text")
+    cardText.classList.add("border")
+    cardText.classList.add("border-info")
+    cardImage.classList.add("card-img-top")
+    cardImage.classList.add("mb-3")
+    cardName.classList.add("mt-2")
+    col.classList.add("col-2")
+    col.classList.add("text-center")
+    card.classList.add("card")
+    card.classList.add("bg-light")
+    cardBody.classList.add("card-body")
+   
+    
+    cardText.appendChild(cardName)
+    cardText.appendChild(cardCode)
+    cardText.appendChild(cardsAmount)
+    cardText.appendChild(cardQuality)
+    cardText.appendChild(cardSleeve)
+
+    cardBody.appendChild(cardImage)
+    cardBody.appendChild(cardText)
+    
+
+    card.appendChild(cardBody)
+    col.appendChild(card)
+    main.appendChild(col)
+}
+
+getAllCards();
+
